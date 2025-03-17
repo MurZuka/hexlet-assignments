eab79a097dc4b60cec6e7bd4a08d3b72b595acf6
MurZuka <murzuka@yahoo.com>
2025-03-17 14:28:04 +0300

    Добавлено решение задания 2

--- java-spring-boot-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/annotations/src/main/java/exercise/Application.java
@@ -6,12 +6,12 @@ import java.lang.reflect.Method;
 
 public class Application {
     public static void main(String[] args) {
-        var address = new Address("London", 12345678);
+        Address address = new Address("London", 12345678);
 
         // BEGIN
-        for (Method method : address.class.getDeclaredMethods()) {
+        for (Method method : address.getClass().getDeclaredMethods()) {
             if (method.isAnnotationPresent(Inspect.class)) {
-                System.out.println("Method " + method.getName() + " returns a value of type " + method.getType());
+                System.out.println("Method " + method.getName() + " returns a value of type " + method.getAnnotatedReturnType());
             }
         }
         // END
