b59a29fc9d799d2cbb8f9c9b81980d1afe9261b7
MurZuka <murzuka@yahoo.com>
2025-03-28 15:04:25 +0300

    Добавлено задание 7 (error-handling) и решение

--- java-spring-boot-ru/jpa-data/src/main/java/exercise/model/Person.java
+++ java-spring-boot-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -3,8 +3,6 @@ package exercise.model;
 import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
